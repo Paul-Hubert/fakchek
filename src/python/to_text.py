7eb9f81ce17a11c4a3b2a4fe77b33a8da031363a
BOURSIN Paul <paul.boursin@lix.polytechnique.fr>
2025-06-25 11:04:05 +0200

    make function

--- src/python/to_text.py
+++ src/python/to_text.py
@@ -1,6 +1,7 @@
 import whisper
 
-model = whisper.load_model("base")  # ou "small", "medium", "large"
-result = model.transcribe("trump_fakcheck.mp4")
-
-print(result["text"])
+def video_to_text(path):
+    model = whisper.load_model("base")  # ou "small", "medium", "large"
+    result = model.transcribe(path)
+    print(result["text"])
+    return result["text"]
